741d08d9c7a8b7a1cd7a3819e7a9298f92fa73ca
Will Krause <wkrause1@radford.edu>
2019-09-06 16:20:36 -0400

    Added controller

---
+++ TodoApi/Controllers/EmptyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace TodoApi.Controllers
+{
+    public class EmptyClass
+    {
+        public EmptyClass()
+        {
+        }
+    }
+}
---
+++ TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+
+            if (_context.TodoItems.Count() == 0)
+            {
+                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+                _context.SaveChanges();
+            }
+        }
+    }
+}
